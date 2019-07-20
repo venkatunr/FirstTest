cd5be2b71bea8e7df69b036642be37c2fe9cd901
venkadeshkumar <venkadesh3125@gmail.com>
2019-07-20 17:46:32 +0530

    Thirt commit - all files

---
+++ src/test/java/gittest/GitTest.java
@@ -0,0 +1,27 @@
+package gittest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	@Test
+	public void loginTest() throws IOException {
+	WebDriver driver=new FirefoxDriver();
+	driver.get("https://www.bing.com/");
+	File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("D:\\WorkSpace\\WorkOut\\File\\screenshot.png"));
+	
+	
+	}
+}
